cee9a37bfe21f2ec3d99de535544cc51f387055b
sofianeyra <safirokpoper@gmail.com>
2019-03-30 18:49:06 -0500

    responsive correction, images added, proyects card, skills and adknowledge items, missing profile image

--- src/js/active.js
+++ src/js/active.js
@@ -2,47 +2,12 @@
   'use strict';
 
   // [ JS Active Code Index ]
-  // :: 1.0 Owl Carousel Active Code
   // :: 2.0 Slick Active Code
   // :: 3.0 Footer Reveal Active Code
   // :: 4.0 ScrollUp Active Code
   // :: 5.0 CounterUp Active Code
   // :: 6.0 onePageNav Active Code
-  // :: 7.0 Magnific-popup Video Active Code
   // :: 8.0 Sticky Active Code
-  // :: 9.0 Preloader Active code
-
-  // :: 1.0 Owl Carousel Active Code
-  if ($.fn.owlCarousel) {
-    $('.welcome_slides').owlCarousel({
-      items: 1,
-      loop: true,
-      autoplay: true,
-      smartSpeed: 1500,
-      nav: true,
-      navText: ['<i class=\'pe-7s-angle-left\'</i>', '<i class=\'pe-7s-angle-right\'</i>']
-    });
-    $('.app_screenshots_slides').owlCarousel({
-      items: 1,
-      loop: true,
-      autoplay: true,
-      smartSpeed: 800,
-      margin: 30,
-      center: true,
-      dots: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        480: {
-          items: 3
-        },
-        992: {
-          items: 5
-        }
-      }
-    });
-  }
 
   // :: 2.0 Slick Active Code
   if ($.fn.slick) {
@@ -89,32 +54,12 @@
     });
   }
 
-  // :: 5.0 CounterUp Active Code
-  if ($.fn.counterUp) {
-    $('.counter').counterUp({
-      delay: 10,
-      time: 2000
-    });
-  }
-
   // :: 6.0 onePageNav Active Code
   if ($.fn.onePageNav) {
     $('#nav').onePageNav({
       currentClass: 'active',
       scrollSpeed: 2000,
       easing: 'easeOutQuad'
-    });
-  }
-
-  // :: 7.0 Magnific-popup Video Active Code
-  if ($.fn.magnificPopup) {
-    $('.video_btn').magnificPopup({
-      disableOn: 0,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: true,
-      fixedContentPos: false
     });
   }
 
